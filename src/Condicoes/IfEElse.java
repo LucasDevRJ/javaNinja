2c095056cba022e8230864057e1abb18fac674e1
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-01-19 17:55:56 -0300

    Condições.

---
+++ src/Condicoes/IfEElse.java
@@ -0,0 +1,34 @@
+package Condicoes;
+
+public class IfEElse {
+    public static void main(String[] args) {
+
+        /*
+            If e Else - Condições
+            Objetivo: Passar o ninja de nível de acordo com o número de missões
+         */
+
+        String nome = "Kakashi";
+        String cla = "Hatake";
+        String rank;
+        boolean hokage = false;
+        short numeroDeMissoes = 80;
+
+        if (numeroDeMissoes <= 30) {
+            rank = "Gennin";
+        } else if (numeroDeMissoes <= 50) {
+            rank = "Chunnin";
+        } else if (numeroDeMissoes <= 100) {
+            rank = "Jounin";
+        } else {
+            hokage = true;
+            rank = "Hokage";
+        }
+
+        System.out.println("Ninja: " + nome);
+        System.out.println("Clã: " + cla);
+        System.out.println("Rank: " + rank);
+        System.out.println("Número de Missões: " + numeroDeMissoes);
+
+    }
+}
