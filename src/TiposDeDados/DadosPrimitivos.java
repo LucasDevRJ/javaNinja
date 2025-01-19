9fac94b4f1278692822c577c4bd17d537cfd0d93
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-01-18 23:04:09 -0300

    Exemplo de dados primitivos.

---
+++ src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,16 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*
+            Dados primitivos - int, double, float, char, boolean, short.
+            Objetivo da aula: criar um ninja - Naruto -
+         */
+
+        int idade = 16;
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 999999L;
+    }
+}
