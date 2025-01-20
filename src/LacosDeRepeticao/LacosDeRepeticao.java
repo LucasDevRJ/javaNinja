e1f096fc29186b5c6bf0b960fd328f217b781e32
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-01-20 20:27:17 -0300

    Exemplo com laços de repetiçaõ.

---
+++ src/LacosDeRepeticao/LacosDeRepeticao.java
@@ -0,0 +1,32 @@
+package LacosDeRepeticao;
+
+public class LacosDeRepeticao {
+    public static void main(String[] args) {
+
+        /*
+            Laços de Repetição: vão repetir infinitamente ou até atingir o parâmetro desejado
+            Exemplos: while, for
+         */
+
+        //while (condição) { Tudo aqui vai acontecer }
+
+//        int numeroDeClones = 0;
+        int numeroMaximoClones = 40;
+
+//        while (numeroDeClones <= numeroMaximoClones) {
+//            numeroDeClones++;
+//            System.out.println("O Naruto fez um clone das sombras");
+//            System.out.println("Número de clones: " + numeroDeClones);
+//        }
+
+        //for
+        for (int i = 1; i < numeroMaximoClones; i++) {
+            String maisDeUmClone = i > 1 ? " vezes" : " vez";
+            System.out.println("O Naruto esta se clonando e já se clonou " + i + maisDeUmClone);
+        }
+
+        for (int i = 2026; i < 3000; i = i + 4) {
+            System.out.printf("Haverá eleição no ano %d\n", i);
+        }
+    }
+}
