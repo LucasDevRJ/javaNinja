2fd82a9aa8acc9fd4c0a4c27e31f478015e397cb
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-01-22 21:30:16 -0300

    Exemplo de array.

---
+++ src/Array/Array.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class Array {
+    public static void main(String[] args) {
+
+        String[] ninjas = new String[4];
+        ninjas[0] = "Naruto Uzumaki";
+        ninjas[1] = "Sasuke Uchiha";
+        ninjas[2] = "Sakura Haruno";
+        ninjas[3] = "kakashi Hatake";
+
+        System.out.println(ninjas[0]);
+        System.out.println(ninjas[1]);
+        System.out.println(ninjas[2]);
+        System.out.println(ninjas[3]);
+
+//        String ninja1 = "Naruto Uzumaki";
+//        String ninja2 = "Sasuke Uchiha";
+//        String ninja3 = "Sakura Haruno";
+//
+//        System.out.println(ninja1);
+//        System.out.println(ninja2);
+//        System.out.println(ninja3);
+    }
+}
