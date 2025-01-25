b0e04dd2a6fa5e6477d8c2411aab39816a2ab4ac
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-01-25 11:44:16 -0300

    Criando os primeiros métodos.

--- src/NivelIntermediario/Main.java
+++ src/NivelIntermediario/Main.java
@@ -14,10 +14,19 @@ public class Main {
         Sasuke.aldeia = "Vila da Folha";
         Sasuke.idade = 18;
 
+        //Aplicando métodos aos meus Objetos
+        Sasuke.SharinganAtivado();
+        String chamandoMetodo = Sasuke.EuSouUmNinja();
+        System.out.println(chamandoMetodo);
+
+        int anosQueFaltamParaSeTornarHokage = Sasuke.anosParaSeTornarHokage(30);
+        System.out.printf("\nO %s tem %d anos de idade, então faltam %d anos para se tornar Hokage.", Sasuke.nome, Sasuke.idade,anosQueFaltamParaSeTornarHokage);
+
         //Criar Sakura Haruno
         Ninja Sakura = new Ninja();
         Sakura.nome = "Sakura Haruno";
         Sakura.aldeia = "Vila da Folha";
         Sakura.idade = 18;
+
     }
 }
--- src/NivelIntermediario/Ninja.java
+++ src/NivelIntermediario/Ninja.java
@@ -4,4 +4,27 @@ public class Ninja {
     String nome;
     String aldeia;
     int idade;
+
+    //Criar um método público personalizado
+
+    /*
+     * O método Void não retorna valor
+     **/
+    public void SharinganAtivado() {
+        System.out.println("O Sharingan Ativou!");
+    }
+
+    /*
+        Método String tem que retornar uma String
+     */
+    public String EuSouUmNinja() {
+        return "Oi, eu sou um ninja!";
+    }
+
+    /*
+        Método int vai retornar um int
+     */
+    public int anosParaSeTornarHokage(int idadeMinimaParaSerHokage) {
+        return idadeMinimaParaSerHokage - idade;
+    }
 }
