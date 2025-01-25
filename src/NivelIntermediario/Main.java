63d697df7d940bf04d99349aafd44e306ccd778d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-01-25 10:52:40 -0300

    Criação da primeira Classe.

---
+++ src/NivelIntermediario/Main.java
@@ -0,0 +1,23 @@
+package NivelIntermediario;
+
+public class Main {
+    public static void main(String[] args) {
+        //Criar o ninja Naruto - Naruto é um Objeto
+        Ninja Naruto = new Ninja();
+        Naruto.nome = "Naruto Uzumaki";
+        Naruto.aldeia = "Vila da Folha";
+        Naruto.idade = 17;
+
+        //Criar ninja Sasuke Uchiha - Sasuke é um Objeto
+        Ninja Sasuke = new Ninja();
+        Sasuke.nome = "Sasuke Uchiha";
+        Sasuke.aldeia = "Vila da Folha";
+        Sasuke.idade = 18;
+
+        //Criar Sakura Haruno
+        Ninja Sakura = new Ninja();
+        Sakura.nome = "Sakura Haruno";
+        Sakura.aldeia = "Vila da Folha";
+        Sakura.idade = 18;
+    }
+}
